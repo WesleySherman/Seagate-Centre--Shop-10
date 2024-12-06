3b0a97f26d5d3247a42e9c6e95033417455cfb60
WesleySherman <wesley.sherman@expsouthafrica.co.za>
2024-12-06 09:10:31 +0200

    Commit

---
+++ seagate-centre---office-10/app-files/data.js
@@ -0,0 +1,299 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-01---office-10---01",
+      "name": "01 - Office 10 - 01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.5494874353016108,
+        "pitch": 0.0434557787718326,
+        "fov": 1.352045691493442
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.23169594796805093,
+          "pitch": 0.291700510866594,
+          "rotation": 0,
+          "target": "2-03---office-10---03"
+        },
+        {
+          "yaw": 0.872445849045155,
+          "pitch": 0.48089572374724554,
+          "rotation": 0,
+          "target": "1-02---office-10---02"
+        },
+        {
+          "yaw": 2.938012890600935,
+          "pitch": 0.3315041573103592,
+          "rotation": 0,
+          "target": "3-06---kitchen-and-bathroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-02---office-10---02",
+      "name": "02 - Office 10 - 02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -1.5338320367512175,
+        "pitch": 0.05817261823240649,
+        "fov": 1.352045691493442
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4595415576124697,
+          "pitch": 0.37960608819895825,
+          "rotation": 0,
+          "target": "2-03---office-10---03"
+        },
+        {
+          "yaw": 3.02223999132217,
+          "pitch": 0.842224846068623,
+          "rotation": 0,
+          "target": "0-01---office-10---01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-03---office-10---03",
+      "name": "03 - Office 10 - 03",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 1.5372207209655953,
+        "pitch": 0.07215015543473058,
+        "fov": 1.352045691493442
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1691142500295433,
+          "pitch": 0.28897043213327933,
+          "rotation": 0,
+          "target": "0-01---office-10---01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-06---kitchen-and-bathroom",
+      "name": "06 - Kitchen and Bathroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.1446183818035962,
+        "pitch": 0.08692556632670012,
+        "fov": 1.352045691493442
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.4150199177826472,
+          "pitch": 0.602186106229194,
+          "rotation": 7.0685834705770345,
+          "target": "0-01---office-10---01"
+        },
+        {
+          "yaw": -2.8802190395898197,
+          "pitch": 0.7400443201437508,
+          "rotation": 4.71238898038469,
+          "target": "4-07---staircase-02-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-07---staircase-02-",
+      "name": "07 - Staircase 02 ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.831209184921935,
+        "pitch": 0.20283120139122168,
+        "fov": 1.352045691493442
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1213815806441545,
+          "pitch": 0.08031466364970186,
+          "rotation": 3.141592653589793,
+          "target": "5-08---staircase-01"
+        },
+        {
+          "yaw": -2.7101987371515754,
+          "pitch": 0.374935978430905,
+          "rotation": 1.5707963267948966,
+          "target": "3-06---kitchen-and-bathroom"
+        },
+        {
+          "yaw": -2.5943655514062822,
+          "pitch": 0.1592052382636684,
+          "rotation": 0,
+          "target": "0-01---office-10---01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-08---staircase-01",
+      "name": "08 - Staircase 01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.8884120210286177,
+        "pitch": 0.0336324702178441,
+        "fov": 1.352045691493442
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.420032717942222,
+          "pitch": 0.3525743175455567,
+          "rotation": 0,
+          "target": "4-07---staircase-02-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Seagate Centre - Office 10",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
